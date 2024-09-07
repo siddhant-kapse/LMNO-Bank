2dbb64230a9a554af1119e1e8c0e9ccf4feb0239
Siddhant Kapse <siddhantkapse4@iitkgp.ac.in>
2024-09-08 01:37:36 +0530

    frontend comp

--- main.py
+++ main.py
@@ -8,5 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/data_extraction')
+def data_extraction():
+    return render_template('data_ext_home.html')
+
+@app.route('/data_extraction/upload_pdf')
+def upload_pdf():
+    return render_template('upload_pdf.html')
+
+@app.route('/data_extraction/text_entry')
+def upload_text():
+    return render_template('upload_text.html')
+
+@app.route('/data_extraction/add_link')
+def upload_link():
+    return render_template('upload_link.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,  port=3000)
